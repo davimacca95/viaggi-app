91efbd82e0993c34f9626168c417a82e040ae5bb
Davide Gino Maccarrone <dmacca@amazon.com>
2026-09-12 21:42:47 +0200

    Quartieri di Tokyo: 50 zone con identikit

--- sw.js
+++ sw.js
@@ -22,28 +22,28 @@
 // Sostituiti a build time. Se restano così, il build non è stato completato.
 const PRECACHE = [
   "./index.html",
-  "./assets/EventRow-4z8JHJbS.js",
-  "./assets/Giorno-YD-YBaZx.js",
-  "./assets/Info-wPX66-Yv.js",
-  "./assets/Luogo-BZoO2SYt.js",
+  "./assets/EventRow-DT57yOTn.js",
+  "./assets/Giorno-g2_If_eJ.js",
+  "./assets/Info-ZbKE8E8e.js",
+  "./assets/Luogo-BOxmNh5S.js",
   "./assets/Mappa-9UNWPgFo.css",
-  "./assets/Mappa-Bdmv5N2L.js",
-  "./assets/Obiettivi-C6vejI0x.js",
-  "./assets/Oggi-CfkT4BVy.js",
-  "./assets/Segno-CraaI--p.js",
-  "./assets/TopBar-DewMnQKI.js",
-  "./assets/Viaggio-WWRJQcBo.js",
-  "./assets/geo-BBCfgP_X.js",
+  "./assets/Mappa-BQbuN5LI.js",
+  "./assets/Obiettivi-C6sC7wKr.js",
+  "./assets/Oggi-CLIeTH7y.js",
+  "./assets/Segno-CDuz2kL7.js",
+  "./assets/TopBar-7bmgbpFQ.js",
+  "./assets/Viaggio-D8CHL0l4.js",
+  "./assets/geo-BNYHXapm.js",
   "./assets/gps-o3sQguSD.js",
-  "./assets/index-4RJK-ff4.css",
-  "./assets/index-Dgef-6yK.js",
+  "./assets/index-BKydEkO8.js",
+  "./assets/index-Bw6aFA3k.css",
   "./assets/maplibre-gl-worker-BDpjEBlB.js",
-  "./assets/nearopen-XwD-YP3O.js",
+  "./assets/nearopen-C_IPRt1c.js",
   "./assets/rail-iuoFzVZ_.js",
   "./assets/style-CcGpYC3U.js",
-  "./assets/time-CtlhHIwm.js",
-  "./assets/tripday-CPgJRG_F.js",
-  "./assets/useNow-BOj3zb5G.js",
+  "./assets/time-CsyNG39n.js",
+  "./assets/tripday-B8YLecTv.js",
+  "./assets/useNow-Bcc2zb1I.js",
   "./data/quartieri-tokyo.geojson",
   "./data/rail-seoul.geojson",
   "./data/rail-tokyo.geojson",
@@ -53,7 +53,7 @@ const PRECACHE = [
   "./icon-512.png",
   "./manifest.webmanifest"
 ]
-const VERSION = "3745d546159b"
+const VERSION = "33897b354ad2"
 
 const CACHE = 'viaggi-' + VERSION
 const SHELL = './index.html'
