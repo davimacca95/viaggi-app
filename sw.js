d827296f32361db5cc9fa20d5ee157515a65ce91
Davide Gino Maccarrone <dmacca@amazon.com>
2026-09-13 15:25:01 +0200

    Il menù nel dettaglio: filtri al sottotipo, e si ricordano

--- sw.js
+++ sw.js
@@ -22,28 +22,28 @@
 // Sostituiti a build time. Se restano così, il build non è stato completato.
 const PRECACHE = [
   "./index.html",
-  "./assets/EventRow-DT57yOTn.js",
-  "./assets/Giorno-g2_If_eJ.js",
-  "./assets/Info-ZbKE8E8e.js",
-  "./assets/Luogo-BOxmNh5S.js",
+  "./assets/EventRow-EiRvW_ak.js",
+  "./assets/Giorno-CAta-4mo.js",
+  "./assets/Info-Ct796Va2.js",
+  "./assets/Luogo-RLxkoe7M.js",
   "./assets/Mappa-9UNWPgFo.css",
-  "./assets/Mappa-BQbuN5LI.js",
-  "./assets/Obiettivi-C6sC7wKr.js",
-  "./assets/Oggi-CLIeTH7y.js",
-  "./assets/Segno-CDuz2kL7.js",
-  "./assets/TopBar-7bmgbpFQ.js",
-  "./assets/Viaggio-D8CHL0l4.js",
-  "./assets/geo-BNYHXapm.js",
+  "./assets/Mappa-DOjnZzha.js",
+  "./assets/Obiettivi-DTMGt-i4.js",
+  "./assets/Oggi-BZi2hFX3.js",
+  "./assets/Segno-CJKZJGu5.js",
+  "./assets/TopBar-B2hwD6AF.js",
+  "./assets/Viaggio-DjWFBtXW.js",
+  "./assets/geo-HWIjjI7m.js",
   "./assets/gps-o3sQguSD.js",
-  "./assets/index-BKydEkO8.js",
-  "./assets/index-Bw6aFA3k.css",
+  "./assets/index-BJ1JyiXl.js",
+  "./assets/index-RQMbrmg7.css",
   "./assets/maplibre-gl-worker-BDpjEBlB.js",
-  "./assets/nearopen-C_IPRt1c.js",
+  "./assets/nearopen-B2aoLOXo.js",
   "./assets/rail-iuoFzVZ_.js",
   "./assets/style-CcGpYC3U.js",
-  "./assets/time-CsyNG39n.js",
-  "./assets/tripday-B8YLecTv.js",
-  "./assets/useNow-Bcc2zb1I.js",
+  "./assets/time-Mp-Fe20F.js",
+  "./assets/tripday-DgpZY345.js",
+  "./assets/useNow-MkwgqRdZ.js",
   "./data/quartieri-tokyo.geojson",
   "./data/rail-seoul.geojson",
   "./data/rail-tokyo.geojson",
@@ -53,7 +53,7 @@ const PRECACHE = [
   "./icon-512.png",
   "./manifest.webmanifest"
 ]
-const VERSION = "33897b354ad2"
+const VERSION = "9ec997160f2a"
 
 const CACHE = 'viaggi-' + VERSION
 const SHELL = './index.html'
